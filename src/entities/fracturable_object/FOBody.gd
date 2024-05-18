6932618ae4ef11a8fb3ac09f9daae3da15e8cd55
Jenny Leidig <jennymaeleidig@gmail.com>
2024-05-18 16:06:12 -0400

    implemented screen dragging, and object boucning

--- src/entities/fracturable_object/FOBody.gd
+++ src/entities/fracturable_object/FOBody.gd
@@ -1,46 +1,41 @@
 extends CharacterBody2D
 
-
-const SPEED = 200
-const JUMP_VELOCITY = -400.0
+@export var BOUNCE_FACTOR = .75
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") # default of 980
+var WINDOW;
+var last_window_position;
+var window_position_change;
+var current_window_position;
 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	WINDOW = get_window();
+	WINDOW.initial_position = WINDOW.WINDOW_INITIAL_POSITION_ABSOLUTE
+	last_window_position = WINDOW.position
+	current_window_position = WINDOW.position
+	window_position_change = Vector2i(0,0)
 
-
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	current_window_position = WINDOW.position
+	window_position_change = current_window_position - last_window_position
+	if (window_position_change != Vector2i(0,0)):
+		print(window_position_change)
+	last_window_position = current_window_position
 
 func _physics_process(delta):
 	
+	velocity.x -= window_position_change.x
+	velocity.y -= window_position_change.y
+	
 	# Add the gravity.
-	print(delta)
+	velocity.y += gravity * delta
+	
 	var collision_info = move_and_collide(velocity * delta)
 	if collision_info:
-		#print(velocity)
-		velocity = velocity.bounce(collision_info.get_normal()) 
-		velocity.y += SPEED
-		#print(velocity)
-	else:
-		velocity.y += gravity * delta
-		
-
+		print(velocity)
+		velocity = velocity.bounce(collision_info.get_normal()) * BOUNCE_FACTOR
+		print(velocity)
 	
-
-	
-
-
-		
-
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction = Input.get_axis("ui_left", "ui_right")
-	#if direction:
-		#velocity.x = direction * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	#move_and_slide()
--- src/scenes/main/main.gd
+++ src/scenes/main/main.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#print_tree_pretty()
 	get_window().initial_position = 0;
 
 
