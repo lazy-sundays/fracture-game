2edbe070f8f971bea4c0e7a247280f9ac9e79cda
Devin Toms <dtoms@vt.edu>
2024-05-19 11:50:22 -0400

    tuned fracture formation

--- src/entities/fracturable_object/FOBody.gd
+++ src/entities/fracturable_object/FOBody.gd
@@ -1,5 +1,7 @@
 extends RigidBody2D
 
+@export var fracture_threshold = 500
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") # default of 980
 var WINDOW;
@@ -7,6 +9,7 @@ var last_window_position;
 var window_position_change;
 var current_window_position;
 var collision_pos = Vector2(0.0, 0.0)
+var speed = Vector2(0, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -27,43 +30,44 @@ func _integrate_forces(state):
 		collision_pos = to_local(state.get_contact_local_position(0))
 
 func _physics_process(delta):
+	speed = linear_velocity
 	apply_force(Vector2(-window_position_change.x, -window_position_change.y))
+	
+func is_in_circle(pt: Vector2, center: Vector2, radius):
+	return ((pt.x - center.x)**2 + (pt.y - center.y)**2 < radius**2)
 	
 func draw_fracture():
 	# find point near center of egg to fracture to
-	var w = %CenterCapsule.shape.radius
-	var h = %CenterCapsule.shape.height / 2
+	var w = max(%TopCapsule.shape.radius, %BottomCapsule.shape.radius)
+	var h = max(%TopCapsule.shape.height / 2, %BottomCapsule.shape.height / 2)
 	var x = randi_range(-w, w)
 	var y = randi_range(-h, h)
 	var inner_pt = Vector2(x, y)
+	while (not is_in_circle(inner_pt, %CenterCircle.position, %CenterCircle.shape.radius)):
+		x = randi_range(-w, w)
+		y = randi_range(-h, h)
+		inner_pt = Vector2(x, y)
 	
-	#calculate fracture path
+	#initialize fracture settings
 	var line = Line2D.new()
 	line.default_color = Color(0, 0, 0)
-	line.width = 1.0
+	line.width = randf_range(0.1, 1.0)
 	
 	var num_fractures = randi_range(5, 20)
 	
+	# build the fracture path and draw
 	line.add_point(collision_pos)
 	for n in range(1, num_fractures):
 		var a = float(n) / num_fractures
-		var normal = (randf_range(-1.0 / num_fractures, 1.0 / num_fractures) * collision_pos.distance_to(inner_pt))
-		var px = ((1 - a) * collision_pos.x) + (a * x) + normal
-		var py = ((1 - a) * collision_pos.y) + (a * y) + normal
-		if px < -w:
-			px = -w;
-		if px > w:
-			px = w;
-		if py < -h:
-			py = -h;
-		if py > h:
-			py = h;
-		print(collision_pos)
-		print(Vector2(px, py))
-		print(inner_pt)
+		var offset = randf_range(-1.0/num_fractures, 1.0/num_fractures) * collision_pos.distance_to(inner_pt)
+		var px = ((1 - a) * collision_pos.x) + (a * x) + offset
+		var py = ((1 - a) * collision_pos.y) + (a * y) + offset
 		line.add_point(Vector2(px, py))
 	line.add_point(inner_pt)
+	
 	add_child(line)
 
 func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
-	draw_fracture()
+	print(speed.length())
+	if (speed.length() > fracture_threshold):
+		draw_fracture()
