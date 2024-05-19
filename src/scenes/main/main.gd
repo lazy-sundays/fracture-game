ee33e126e59b22a3c502b543d333d703782f73ee
Jenny Leidig <jennymaeleidig@gmail.com>
2024-05-19 18:15:55 -0400

    added sounds

--- src/scenes/main/main.gd
+++ src/scenes/main/main.gd
@@ -1,10 +1,17 @@
 extends Node2D
 
-
+var music_player;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#print_tree_pretty()
-	pass
+	music_player = AudioStreamPlayer.new()
+	add_child(music_player)
+	var music = load("res://src/globals/assets/panic.mp3")
+	music.set_loop(true)
+	music_player.set_stream(music)
+	music_player.volume_db = -15.0
+	music_player.play()
+	
 
 
 
